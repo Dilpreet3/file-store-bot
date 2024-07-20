34675cae7de427301ef82f4843886b4c3811a7f4
Dilpreet3 <tejasingh7786@gmail.com>
2024-07-20 07:17:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ def is_enabled(value, default):
 
       
 # Owner Information
-API_ID = int(environ.get("API_ID", "29640476"))
+API_ID = int(environ.get("API_ID", "28569174"))
 API_HASH = environ.get("API_HASH", "824d5b93ac8c8fddcf4088a67ecd5222")
 ADMINS = int(environ.get("ADMINS", "6138367360"))
 
